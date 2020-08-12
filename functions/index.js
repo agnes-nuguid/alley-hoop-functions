bcade48c7ffc6fbb8b5ccc631a7b0f2b1ba49e91
Agnes Nuguid <agnes.nuguid@uap.asia>
2020-08-12 16:37:20 +0800

    #1: Live Games - Update todayScoreboardLink from today.json

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,13 @@ const functions = require("firebase-functions");
 
 const app = require("express")();
 
-const { getTodayScoreboardLink } = require("./routes/games");
+const {
+  getTodayScoreboardLink,
+  updateTodayScoreboardLink,
+} = require("./routes/games");
 
 app.get("/games/todayScoreboardLink", getTodayScoreboardLink);
+app.put("/games/todayScoreboardLink", updateTodayScoreboardLink);
 
 // Export base url with /api
 // Use asia-east2 since Hong Kong is nearest
--- functions/routes/games.js
+++ functions/routes/games.js
@@ -1,4 +1,5 @@
 const { db } = require("../util/db");
+const axios = require("axios");
 
 exports.getTodayScoreboardLink = (req, res) => {
   return db
@@ -9,6 +10,29 @@ exports.getTodayScoreboardLink = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      response.status(500).json({ error: err.code });
+      res.status(500).json({ error: err.code });
     });
+};
+
+exports.updateTodayScoreboardLink = async (req, res) => {
+  try {
+    //Gets the Today details
+    const todayResponse = await axios.get(
+      "https://data.nba.net/10s/prod/v4/today.json"
+    );
+
+    const todayScoreboard =
+      "https://data.nba.net/10s" + todayResponse.data.links.todayScoreboard;
+
+    return db.ref().update({ todayScoreboard }, function (error) {
+      if (error) {
+        res.json({ error: error });
+      } else {
+        res.json(`todayScoreboard updated to ${todayScoreboard} successfully`);
+      }
+    });
+  } catch (e) {
+    console.log("tryCatche", e);
+    res.sendStatus(400);
+  }
 };
